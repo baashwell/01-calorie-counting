8a61166125629a471af975cfc068cdad55108838
Ben Ashwell <ben.ashwell@madetech.com>
2022-12-07 09:27:34 +0000

    Add sum for multiple food items

--- get_elves_calories.go
+++ get_elves_calories.go
@@ -1,5 +1,14 @@
 package calorie_counting
 
-func GetElvesCalories(elves_calories_list []string) int {
-	return 1000
+import "strconv"
+
+func GetElvesCalories(food_items []string) int {
+	total_calories := 0
+
+	for _, food_item := range food_items {
+		calories, _ := strconv.Atoi(food_item)
+		total_calories += calories
+	}
+
+	return total_calories
 }
--- get_elves_calories_test.go
+++ get_elves_calories_test.go
@@ -14,4 +14,12 @@ func TestGetElvesCalories(t *testing.T) {
 			t.Errorf("expected %q result %q", strconv.Itoa(expected), strconv.Itoa(result))
 		}
 	})
+	t.Run("Test Get Elves Calories for elf with two food items returns total calories for elf", func(t *testing.T) {
+		expected := 6000
+		result := GetElvesCalories([]string{"1000", "5000"})
+
+		if expected != result {
+			t.Errorf("expected %q result %q", strconv.Itoa(expected), strconv.Itoa(result))
+		}
+	})
 }
